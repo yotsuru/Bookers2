4edee2a4ec22169d3694f960bbd5aaf25903e8c9
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-07-05 13:37:17 +0000

    [Add]Bookモデル・テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_05_115605) do
+ActiveRecord::Schema.define(version: 2023_07_05_131458) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "opinion"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
