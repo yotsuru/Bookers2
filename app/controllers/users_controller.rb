418b3234e0dbe89a1f76de53c599e4dc187991b0
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-07-14 11:02:52 +0000

    [Add]bookのeditページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,8 @@ class UsersController < ApplicationController
 
   def edit
   end
+  
+  def index
+    @users = User.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
+ 
+  get 'users/index'
   resources :books, only: [:new, :index, :show, :edit]
   devise_for :users
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'books' => 'books#create'
   root to: "homes#top"
