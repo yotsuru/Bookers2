c4015f1e4e44a5653025bb80d16e064db2fa1fcf
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-07-18 08:59:25 +0000

    [Add]Userのeditページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   
+  def index
+    @users = User.all
+  end
+  
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -9,7 +13,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-  def index
-    @users = User.all
+  def update
+   @user = User.find(params[:id])
+   @user.update(user_params)
+   redirect_to user_path(current_user.id)
   end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
  
-  get 'users/index'
+  #get 'users/index'
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
   devise_for :users
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post 'books' => 'books#create'
+  #post 'books' => 'books#create'
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: "about"
 end
