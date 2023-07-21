1b25edd0e4bd4f28b919fffb663bd3808d6200eb
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-07-21 14:56:49 +0000

    [Add]ユーザの情報更新のバリデーション”
    git push origin main
    
    git commit -m  [Add]ユーザの情報更新のバリデーション”

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
     redirect_to user_path(current_user.id), notice: "You have updated user successfully."
+    else
+      render :edit
     end
   end
 
