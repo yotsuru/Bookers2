69c5ac9a392a92991ea9bd3e534fa1fed1f07a16
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-07-22 15:54:49 +0000

    [Add]他ユーザからのアクセス制限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,10 @@ class BooksController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+    redirect_to books_path
+    end
     @book = Book.find(params[:id])
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,10 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+    redirect_to users_path
+    end
     @user = User.find(params[:id])
   end
   
