aaec08e541c6bf46f5e9df9765799bdb75e5c889
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-07-27 19:00:49 +0000

    [Add]RSpec,[Update]Userモデルのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
  
   has_one_attached :profile_image
   
-  validates :name, presence: true
+  validates :name, length: { minimum: 2, maximum: 20 },uniqueness:true
+  validates :introduction, length: { maximum: 50 }
   
   def get_profile_image(width, height)
   unless profile_image.attached?
