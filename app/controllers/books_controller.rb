b7daed028222ef0e413c6a76f3ffc289895ab1f9
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-07-27 20:27:34 +0000

    [Update]Bookモデルのバリデーション,各所のカラム名修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -46,6 +46,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title,  :bo)
+    params.require(:book).permit(:title,  :body)
   end 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,21 +3,21 @@ class Book < ApplicationRecord
     belongs_to :user
     
     validates :title, presence: true
-    validates :body, presence: true,length: { minimum: 1, maximum: 200 }
+    validates :body, presence: true,length: { maximum: 200 }
     
-  def get_image
+   def get_image
     if image.attached?
       image
     else
       'no_image.jpg'
     end
-  end
-
-  def get_image
+   end
+   
+   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
-  end
+   end
 end
